a823273b11821c1585df4c3d3e4319014f61fdc7
Sam Siu <sam.siu@rollerdigital.com>
2017-06-27 15:58:17 +1000

    init

---
+++ AppVeyor.Web/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppVeyor.Web.Startup))]
+namespace AppVeyor.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
